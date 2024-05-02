78ae39e9233c34247880466ac16b14fb7ec05dde
azure2_gerry <gerrytech567@gmail.com>
2024-05-02 10:34:32 +0700

    feat(js): 数据运算

---
+++ 技术应用阶段/JS收官/数据的运算.js
@@ -0,0 +1,15 @@
+/**
+ * 编写一个完美的求和函数：
+ * 1.若两个数据都是数字，则返回两个数字的和
+ * 2.NaN的数据需要转成0
+ * 3.其他类型的数据需要转成数字
+ */
+
+function sum(a, b) {
+  // +a 转换成数字
+  a = +a || 0;
+  b = +b || 0;
+  return a + b;
+}
+
+console.log(sum(undefined, undefined)); // 3
