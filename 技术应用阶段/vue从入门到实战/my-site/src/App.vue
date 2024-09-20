f14dbab7dfa0662203914e32996f5cfbeda690b1
cheer001 <gerrycheer@gmail.com>
2024-09-20 14:04:06 +0700

    feat(vue入门): 封装Contact组件

--- 技术应用阶段/vue从入门到实战/my-site/src/App.vue
+++ 技术应用阶段/vue从入门到实战/my-site/src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <div id="app">
     <h1>APP组件</h1>
-    <Pager />
+    <!-- <Pager />
     <div class="test-empty">
       <Empty />
+    </div> -->
+    <div class="test-image-loader">
+      <ImageLoader
+        :src="image1"
+        :placeholder="image2"
+        :duration="1000"
+        @load="handleLoaded"
+      />
     </div>
   </div>
 </template>
@@ -11,21 +19,29 @@
 <script>
 import Pager from "./components/Pager/index.vue";
 import Empty from "./components/Empty/index.vue";
+import ImageLoader from "./components/ImageLoader/index.vue";
 
 export default {
   name: "App",
   components: {
     Pager,
     Empty,
+    ImageLoader,
   },
   data() {
     return {
       firstName: "John",
       lastName: "Doe",
+      image1: require("./components/ImageLoader/origin.png"),
+      image2: require("./components/ImageLoader/placeholder.png"),
     };
   },
   computed: {},
-  methods: {},
+  methods: {
+    handleLoaded() {
+      console.log("image loaded");
+    },
+  },
 };
 </script>
 
@@ -36,9 +52,17 @@ export default {
 }
 
 .test-empty {
+  border: 1px solid red;
   width: 500px;
   height: 500px;
   background-color: #f0f0f0;
   position: relative;
+}
+
+.test-image-loader {
+  width: 800px;
+  height: 600px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
 }
 </style>
---
+++ 技术应用阶段/vue从入门到实战/my-site/src/components/Contact/index.vue
@@ -0,0 +1,121 @@
+<template>
+  <ul class="contact-container">
+    <li>
+      <a href="">
+        <div class="icon">
+          <Icon name="home" />
+        </div>
+        <span>231231231</span>
+      </a>
+      <div class="pop">
+        <img
+          src="https://ai.bdstatic.com/file/1E862211BBC64890A993557E222A4A57"
+          alt=""
+        />
+      </div>
+    </li>
+    <li>
+      <a href="">
+        <div class="icon">
+          <Icon name="about" />
+        </div>
+        <span>gerrymail@gmail.com</span>
+      </a>
+      <div class="pop">
+        <img
+          src="https://ai.bdstatic.com/file/1E862211BBC64890A993557E222A4A57"
+          alt=""
+        />
+      </div>
+    </li>
+  </ul>
+</template>
+<script>
+import Icon from "../Icon/index.vue";
+export default {
+  name: "Contact",
+  components: {
+    Icon,
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+<style lang="less" scoped>
+.contact-container {
+  width: 100%;
+  height: 300px;
+  background-color: transparent;
+  list-style: none;
+  padding: 0;
+  @itemHeight: 30px;
+  li {
+    height: @itemHeight;
+    line-height: 30px;
+    margin: 14px 0;
+    cursor: pointer;
+    position: relative;
+    padding: 0 5px;
+    &:hover .pop {
+      display: block;
+      transform: scaleY(1);
+    }
+  }
+  a {
+    display: flex;
+    align-items: center;
+    text-decoration: none;
+    color: #333;
+  }
+  .icon {
+    width: @itemHeight;
+    height: @itemHeight;
+    font-size: 20px;
+    margin-right: 5px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: aliceblue;
+    .icon-container {
+      font-size: 20px !important;
+    }
+  }
+  .pop {
+    position: absolute;
+    width: 150px;
+    height: 150px;
+    line-height: 150px;
+    background-color: #fff;
+    top: -150px;
+    left: 10%;
+    border-radius: 5px;
+    text-align: center;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    transform: scaleY(0);
+    transition: transform 0.3s;
+    transform-origin: bottom;
+
+    img {
+      width: 140px;
+      height: 140px;
+      object-fit: cover;
+      display: inline;
+    }
+    // 三角形 从上到下
+    &::after {
+      content: "";
+      position: absolute;
+      width: 0;
+      height: 0;
+      border: 10px solid transparent;
+      border-top-color: #fff;
+      top: 150px;
+      left: 50%;
+      transform: translateX(-50%);
+    }
+  }
+}
+</style>
