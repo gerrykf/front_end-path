5dc65138dc5a16d8564d2f78a8664ee9d7feffc8
gerrydev <gerrydeveloper66@gmail.com>
2024-12-04 02:12:39 +0700

    feat(monorepo): 添加公用库测试案例

---
+++ 架构师技术栈/包管理方案/Monorepo工程/frontend-project/projects/tools-test-proj/src/index.ts
@@ -0,0 +1,12 @@
+import { sum, sub } from "tools";
+
+export function testSum() {
+  return sum(1, 2);
+}
+
+export function testSub() {
+  return sub(2, 1);
+}
+
+console.log("test-sum", testSum());
+console.log("test-sub", testSub());
