c4caab3d1bcfc21a6fd0e2239d73d4e18bf921ea
gerrydev <gerrydeveloper66@gmail.com>
2024-12-04 01:25:50 +0700

    fix:[!] TypeError: Unknown file extension ".ts" for rollup.config.js

--- 架构师技术栈/包管理方案/Monorepo工程/frontend-project/utils/tools/rollup.config.ts
+++ 架构师技术栈/包管理方案/Monorepo工程/frontend-project/utils/tools/rollup.config.ts
@@ -7,4 +7,68 @@ import resolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import babel from "@rollup/plugin-babel";
 
-export default [];
+const extensions = [".js", ".ts"];
+
+// 导出一个数组，数组里面每一项对应一种格式的配置
+export default [
+  // CommonJS
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.cjs",
+      format: "cjs",
+    },
+    plugins: [
+      typescript({
+        useTsconfigDeclarationDir: true,
+      }),
+      resolve({ extensions }),
+      commonjs(),
+      json(),
+    ],
+  },
+  // ESM
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.js",
+      format: "es",
+    },
+    plugins: [
+      typescript({
+        useTsconfigDeclarationDir: true,
+      }),
+      resolve({ extensions }),
+      commonjs(),
+      json(),
+    ],
+  },
+  // Browser-compatible
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.browser.js",
+      format: "iife",
+      name: "MyModule",
+    },
+    plugins: [
+      typescript({
+        useTsconfigDeclarationDir: true,
+      }),
+      resolve({ extensions }),
+      commonjs(),
+      json(),
+      babel({
+        exclude: "node_modules/**",
+        extensions,
+        babelHelpers: "bundled",
+        presets: [
+          ["@babel/preset-env"],
+          {
+            targets: "> 0.25%, not dead",
+          },
+        ],
+      }),
+    ],
+  },
+];
