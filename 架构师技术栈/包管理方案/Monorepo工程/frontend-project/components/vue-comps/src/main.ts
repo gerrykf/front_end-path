fa3bca417a37e6131ee2fec9840bf4194ba15e4b
cheer001 <gerrycheer@gmail.com>
2024-12-04 15:37:47 +0700

    feat(monorepo): 组件库的开发

---
+++ 架构师技术栈/包管理方案/Monorepo工程/frontend-project/components/vue-comps/src/main.ts
@@ -0,0 +1,16 @@
+import './assets/main.css';
+import './styles/normalize.scss';
+
+import { createApp } from 'vue';
+import App from './App.vue';
+
+// 引入自定义组件
+import Button from './components/Button.vue';
+import Card from './components/Card.vue';
+
+// 全局注册组件
+
+const app = createApp(App);
+app.component(Button.name, Button);
+app.component(Card.name, Card);
+app.mount('#app');
